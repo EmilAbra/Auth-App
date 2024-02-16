2b974b0e68b97477028e786144e39233fe7ab515
eabra <emab21@student.bth.se>
2024-02-16 18:53:33 +0100

    Add Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import Signup from './Signup';
+import Dashboard from './Dashboard';
+import Login from './Login';
+import PrivateRoute from './PrivateRoute';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../contexts/authContext';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <AuthProvider>
+  return (    
       <Container 
         className='d-flex align-items-center justify-content-center'
         style={{ minHeight: "100vh" }}
       >
-      <div className='w-100' style={{ maxWidth: "400px" }}>
-        <Signup />
-      </div>
+        <div className='w-100' style={{ maxWidth: "400px" }}>
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route path="/"
+                  element={
+                    <PrivateRoute>
+                      <Dashboard />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path='/signup' Component={Signup} />
+                <Route path='/login' Component={Login} />
+              </Routes>
+            </AuthProvider>
+          </Router>
+        </div>
       </Container>
-    </AuthProvider>
   );
 }
 
